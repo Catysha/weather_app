19dbb902fd8e1d9107492006ee439565630bab61
Catysha <viktorekaterina2005@gmail.com>
2025-11-23 16:08:58 +0300

    Add react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import {Route, Routes} from "react-router-dom";
+import {Home} from "./pages/Home/components/Home";
+import {MonthStatistics} from "./pages/MonthStatictics/components/MonthStatistics";
 
 function App() {
   return (
-    <div className="App"></div>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/month-statictics" element={<MonthStatistics />}/>
+        </Routes>
+      </div>
   );
 }
 
