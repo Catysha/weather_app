a2c08771f4fcedfcbbdf8dde17eb8d4081346577
Catysha <viktorekaterina2005@gmail.com>
2025-11-24 15:31:07 +0300

    feat: add component ThisDay with styles

--- src/pages/Home/components/Home.tsx
+++ src/pages/Home/components/Home.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import s from "./Home.module.scss";
+import {ThisDay} from "./ThisDay/ThisDay";
 
 interface Props {
 
@@ -7,6 +8,6 @@ interface Props {
 
 export const Home = (props: Props) => {
     return (<div className={s.home}>
-
+        <ThisDay/>
     </div>)
 }
---
+++ src/pages/Home/components/ThisDayInfo/ThisDayInfo.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import s from "./ThisDayInfo.module.scss";
+
+interface Props {
+
+}
+
+export const Header = (props: Props) => {
+    return (<header className={s.header}></header>)
+}
