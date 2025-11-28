45ddc2d81e0fffdf8bb1a5e44d2afc5ec3de4376
Catysha <viktorekaterina2005@gmail.com>
2025-11-28 16:31:28 +0300

    feat: add component Popup with styles and finish layout

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,19 @@ import {Route, Routes} from "react-router-dom";
 import {Home} from "./pages/Home/components/Home";
 import {MonthStatistics} from "./pages/MonthStatictics/components/MonthStatistics";
 import {Header} from "./shared/Header/Header";
+import {Popup} from "./shared/Popup/Popup";
 
 function App() {
   return (
-      <div className="container">
-          <Header/>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/month-statictics" element={<MonthStatistics />}/>
-        </Routes>
+      <div className="global-container">
+          <Popup/>
+          <div className="container">
+              <Header/>
+              <Routes>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/month-statictics" element={<MonthStatistics />}/>
+              </Routes>
+          </div>
       </div>
   );
 }
